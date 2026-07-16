04cd28f81ccd07c0a231ef75aff6ce47451cded2
Laís Freitas <135891577+freitaslais@users.noreply.github.com>
2026-07-16 00:59:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h1 = document.querySelector("h1");
 const animaisLista = document.querySelector(".animais-descricao");
 
-h1.innerHTML = "<p>Nina sua linda</p>";
+h1.innerHTML = "<p>Animais Fantasticos</p>";
 
 console.log(animaisLista.innerHTML);
